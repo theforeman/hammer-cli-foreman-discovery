5450ca8d4c4f6226c59ec4da0dd12a659a8d5d2c
Ori Rabin <orabin@redhat.com>
2015-01-07 18:48:58 +0200

    Fixes #8819 - Added facts command with table output

--- test/unit/discovery_resource_mock.rb
+++ test/unit/discovery_resource_mock.rb
@@ -8,8 +8,21 @@ module DiscoveryResourceMock
 
   def self.discovered_hosts_show
     ResourceMocks.mock_action_calls(
-        [:discovered_hosts, :index, [{ "id" => 2, "name" => "mac52540068f9d6" }]],
-        [:discovered_hosts, :show, { "id" => 2, "name" => "mac52540068f9d6" }]
+      [:discovered_hosts, :index, [{ "id" => 2, "name" => "mac52540068f9d6" }]],
+      [:discovered_hosts, :show, { "id" => 2, "name" => "mac52540068f9d6" }]
     )
+  end
+
+  def self.facts_index
+    ResourceMocks.mock_action_call(:discovered_hosts, :show, {
+      "facts_hash" => {
+        "network_br180"=>"10.32.83.0",
+        "mtu_usb0"=>"1500",
+        "physicalprocessorcount"=>"1",
+        "rubyversion"=>"1.8.7"
+      },
+      "id" => 2,
+      "name" => "mac52540068f9d6"
+    })
   end
 end
